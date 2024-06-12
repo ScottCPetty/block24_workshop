e3d02358fc4923f2eb21061aa0176011e1724dcf
ScottCPetty <scottishcheese13@gmail.com>
2024-06-12 12:34:28 -0400

    Added more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import {puppyList} from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies)
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        <p>{featPupId && <p>{featPupId}</p>}</p>
       </div>
     </>
   );
